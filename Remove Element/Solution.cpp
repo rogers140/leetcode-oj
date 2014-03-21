2cfbfd1dfe8918e33f0acd7215091d10c73aa6b6
rogers140 <rogers140@gmail.com>
2014-03-21 21:15:30 +0800

    add json test file for volley test

--- Remove Element/Solution.cpp
+++ Remove Element/Solution.cpp
@@ -5,11 +5,24 @@ using namespace std;
 class Solution {
 public:
 	int removeElement(int A[], int n, int elem) {
+		//the first is a little tricky.
+		int count = 0;
+		for(int i = 0; i < n; ++i) {
+			if(A[i] == elem) {
+				++count;
+			}
+			else if(count > 0) {
+				A[i - count] = A[i];
+			}
+		}
+		return n - count;
+		/* 
+		// The second method is much easier to understand.
+		// Put all elements that should be delete to the end of the array.
 		int front = 0;
 		int back = n - 1;
 		int length = n;
 		while(front <= back) {
-			//put all the elem into the back of the array
 			if(A[front] == elem) {
 				--length;
 				while((A[back] == elem) && (front < back)) { 
@@ -31,13 +44,15 @@ public:
 			}
 		}
 		return length;
+		*/
+
 	}
 };
 
 int main(int argc, char const *argv[]) {
 	Solution *s = new Solution();
-	int array[] = {3,4};
-	int length = s->removeElement(array, 2, 4);
+	int array[] = {3,4,5,6,4,4,6};
+	int length = s->removeElement(array, 7, 4);
 	for(int i = 0; i < length; ++i) {
 		cout<<array[i]<<",";
 	}
