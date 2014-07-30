4634bdb7bbbff968f9f55794398979028b33e25c
rogers140 <rogers140@gmail.com>
2014-07-30 21:41:50 +0800

    fix the composition

--- Anagrams/Solution.cpp
+++ Anagrams/Solution.cpp
@@ -10,14 +10,14 @@ public:
     vector<string> anagrams(vector<string> &strs) {
     	vector<string> vec,anavec;
     	unordered_map<string,vector<string> > mp;
-        for(string s:strs){                    //sort every string and store groups of strings that are anagrams in a map
-            string tmp=s;
-            sort(tmp.begin(),tmp.end());
+        for(string s : strs){                    //sort every string and store groups of strings that are anagrams in a map
+            string tmp = s;
+            sort(tmp.begin(), tmp.end());
             mp[tmp].push_back(s);
         }
-        for(auto map:mp){                      // find all anagrams
-            if(map.second.size()>1)
-            	vec.insert(vec.end(),map.second.begin(),map.second.end());
+        for(auto map : mp){                      // find all anagrams
+            if(map.second.size() > 1)
+            	vec.insert(vec.end(), map.second.begin(), map.second.end());
         }
         return vec;
     }
