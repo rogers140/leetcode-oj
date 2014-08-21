6b4549a673336f4f58163f57452eab81f242c574
rogers140 <rogers140@gmail.com>
2014-08-21 20:27:51 +0800

    begin to solve Minimun Window Subtring

---
+++ Minimum Window Substring/Solution.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string minWindow(string S, string T) {
+         
+    }
+};
+int main(int argc, char const *argv[])
+{
+	
+	
+	return 0;
+}
