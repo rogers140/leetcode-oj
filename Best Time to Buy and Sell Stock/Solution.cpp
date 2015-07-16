af812cf9c1abb01ea2c78c83e26d75531c63bc0f
rogers140 <rogers140@gmail.com>
2015-07-15 22:31:47 -0700

    best time to buy and sell stock

---
+++ Best Time to Buy and Sell Stock/Solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int minPrice = INT_MAX;
+        for (int p : prices) {
+        	minPrice = std::min(minPrice, p);
+        	maxProfit = std::max(p - minPrice, maxProfit);
+        }
+        return maxProfit;
+    }
+};
+int main(int argc, char const *argv[])
+{
+	int price_list[8] = {3, 2, 4, 9, 7, 1, 8, 6};
+	vector<int> prices(&price_list[0], &price_list[7]);
+	Solution *s = new Solution();
+	cout<<s->maxProfit(prices)<<endl;
+	return 0;
+}
