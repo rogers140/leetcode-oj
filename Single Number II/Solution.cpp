7c28ff5afab67f056e8111f4c159d3e3c129f4e6
rogers140 <rogers140@gmail.com>
2015-08-22 12:06:43 -0700

    Single number II

--- Single Number II/Solution.cpp
+++ Single Number II/Solution.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-// Uses xor
 class Solution {
 public:
 	int singleNumber(vector<int>& nums) {
-		const int size  = nums.size();
-		int single = 0;
-		for (int i = 0; i < size; ++i) {
-			single ^= nums[i];
-		}
-		return single;
+		int b1 = 0;
+    int b0 = 0;
+    for (int num : nums){
+      int nb0 = (~b1 & b0 & ~num) | (~b1 & ~b0 & num);
+      int nb1 = (b1 & ~b0 & ~num) | (~b1 & b0 & num) ;
+			b0 = nb0;
+      b1 = nb1;
+    }
+    return b0;
 	}
 };
 int main(int argc, char const* argv[]) {
-	int num_list[] = {2, 1, 2, 1, 3, 0, 0};
+	int num_list[] = {2, 1, 2, 2, 3, 3, 3};
 	vector<int> nums(num_list, num_list + 7);
 	Solution *s = new Solution();
 	cout<<s->singleNumber(nums)<<endl;
