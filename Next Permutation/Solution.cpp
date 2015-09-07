9213cc7cbaefff845225ae6a54db06ce382f4fa9
rogers140 <rogers140@gmail.com>
2015-09-07 14:15:06 -0700

    Add some old solution

--- Next Permutation/Solution.cpp
+++ Next Permutation/Solution.cpp
@@ -7,18 +7,18 @@ public:
 	void nextPermutation(vector<int>& nums) {
 		if (nums.size() == 0 || nums.size() == 1) return;
 		int endOfIncrease = 0;
-		for (int i = 1; i < nums.size(); ++i) {
-			if (nums[i - 1] < nums[i])
+		for (int i = 0; i < nums.size() - 1; ++i) {
+			if (nums[i] < nums[i + 1])
 				endOfIncrease = i;
 		}
 		if (endOfIncrease > 0) {
 			int swapIndex = nums.size() - 1;
 			while (swapIndex > endOfIncrease - 1 &&
-				     nums[swapIndex] <= nums[endOfIncrease - 1])
+				     nums[swapIndex] <= nums[endOfIncrease])
 				--swapIndex;
-			swap(nums[endOfIncrease - 1], nums[swapIndex]);
+			swap(nums[endOfIncrease], nums[swapIndex]);
 		}
-		sort(nums.begin() + endOfIncrease, nums.end());
+		sort(nums.begin() + endOfIncrease + 1, nums.end());
 	}
 };
 int main(int argc, char const *argv[])
